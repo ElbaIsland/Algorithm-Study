81d07b4170b1ada370a30ca5076c7f8bd6017dd3
name <phr038942@gmail.com>
2021-08-15 01:46:15 +0900

    week2dayfinal_2

---
+++ src/week01/MostCommonWord.java
@@ -0,0 +1,47 @@
+package week01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+    	
+    	for (int i = 0; i < banned.length; i++) {
+    		paragraph = paragraph.replace(banned[i], "");
+		}
+    	
+    	String[] checkWord = paragraph.replaceAll("[!?;\\.\\,]", "").toLowerCase().split(" ");
+
+    	Map<String, Integer> wordCnt = new HashMap<String, Integer>();
+    	int maxCount = 0;
+    	int count = 0;
+    	String maxWord = "";
+    	
+    	for (int i = 0; i < checkWord.length; i++) {
+    		
+    		wordCnt.put(checkWord[i], wordCnt.getOrDefault(checkWord[i], 0)+1);
+    		count = wordCnt.get(checkWord[i]);
+    		
+    		if (maxCount < count && !checkWord[i].isEmpty()) {
+				maxCount = count;
+				maxWord = checkWord[i];					
+			}
+    	
+		}
+    	
+    	System.out.println("maxWord : " + maxWord);
+        return maxWord;   
+    }
+    
+    
+    public static void main(String[] args) {
+		
+    	String[] bannedWord = {"Bob", "hIt"};
+    	
+    	MostCommonWord MCW = new MostCommonWord();
+    	
+    	MCW.mostCommonWord("Bob. hIt, baLl", bannedWord);
+    	
+	}
+}
