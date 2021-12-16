38e513581063418fce7cf36e004e460bf378fd13
name <phr038942@gmail.com>
2021-12-16 21:59:10 +0900

    week12

--- week12/CheckTheDistance.java
+++ week12/CheckTheDistance.java
@@ -1,92 +1,78 @@
 package week12;
 
-import java.util.*;
-
-// 1. 대기실의 모든 자리를 탐색하면서, P(응시자의 위치)를 찾는다.
-// 2. bfs(사방탐색)를 통해, 맨해튼거리 2 이내에(최근접 사방향) 다른 P가 있는지 확인
-// 3. 각 거리별 경우의 수를 탐색(최근접거리 기준)
-//     1) P가 있다면 FALSE(0) RETURN
-//     2) X가 있다면 현재 방향 탐색 필요 X(모든 경우의 수 충족)
-//     3) O가 있다면 최근접거리에 같은 O 또는 X만 가능 => 탐색 한번 더 진행되야 함
-
-// 두 테이블 T1, T2가 행렬 (r1, c1), (r2, c2)에 각각 위치하면,
-// T1, T2 사이의 맨해튼 거리는 |r1 - r2| + |c1 - c2|
-// 공식 : int manhaten = Math.abs(nextr - r) + Math.abs(nextc - c); (abs : 절대값 )
-// 현재 응시자를 기준으로 x축/y축 느낌으로 이해하자!
 
 public class CheckTheDistance {
-    
+	
+	//dfs 조건 
+	//사람과 사람사이에 파티션 존재 하면 ok 
+	//한사람 기준으로 -> 사방에 파티션 있으면 ok,
+	// 만약 사방에 파티션이 없는데 
+	//빈테이블은 무시이다.. 그냥 공간.
+	//int [][] dp = int 	
+	
+	
     public int[] solution(String[][] places) {
-        
-        int[] answer = new int[places.length];
-        
-        for(int i = 0; i < places.length; i++){
-            
-             boolean isOk = true; // 각 대기실별 clean 여부
-             String[] perRoom = places[i];
-          //   System.out.println(Arrays.toString(perRoom));  // 대기실 목록
-            
-             for(int j = 0; j < perRoom.length; j++){
-                 
-                 String perRow = perRoom[j];
-             //    System.out.println("perRow : " + perRow); // 각 row별 목록
-                 
-                 for (int x = 0; x < 5; x++) {
-                     
-                    if (perRoom[j].charAt(x) == 'P') {  //  그 줄에 앉아있는 사람이 있다면
-                        
-                        //  그 사람을 기준으로 bfs 실행
-                        //  j : 방의 각 행 | x : 방의 각 열
-                        if (!bfs(j, x, perRoom)){  
-                            isOk = false; // bfs를 빠져나오면 false chk
-                        }
-                    }
-                     
-                 } // for 3 end                
-   
-             } // for 2 end
-            
-           answer[i] = isOk ? 1 : 0;    //  해당 대기실의 clean여부 chk
-        } // for 1 end
-        
+        int len = places.length;
+        int[] answer = new int[len];
+        for(int i = 0; i < len; i++){
+            String[][] room = new String[5][5];
+            for(int z = 0 ; z <  places[i].length; z++){
+                room[z] = places[i][z].split("");   
+            }
+            answer[i] = dfs(room);
+        } 
         return answer;
-    } // solution end
+    }
+    
+    Boolean check(String[][] room, int row, int column){
+        // 디피 넣기 !!!!!!!!!!!!!!!!!
+        if(!room[row][column].equals("P")){// 사람아닌건 pass
+            return true;
+        }
+
+        if(row != 4 && room[row+1][column].equals("P")) return false;
+        if(row != 0 && room[row-1][column].equals("P")) return false;
+        if(column != 4 && room[row][column+1].equals("P")) return false;
+        if(column != 0 && room[row][column-1].equals("P")) return false;
+        
+        if(row != 4 && column != 4  && room[row+1][column+1].equals("P")){
+            if( !room[row+1][column].equals("X") || !room[row][column+1].equals("X"))
+            return false;
+        }
+        if(row != 4 && column != 0  && room[row+1][column-1].equals("P")){
+            if( !room[row+1][column].equals("X") || !room[row][column-1].equals("X"))
+            return false;
+        }
+        if(row != 0 && column != 4  && room[row-1][column+1].equals("P")){
+            if( !room[row-1][column].equals("X") || !room[row][column+1].equals("X"))
+            return false;
+        }
+        if(row != 0 && column != 0  && room[row-1][column-1].equals("P")){
+            if( !room[row-1][column].equals("X") || !room[row][column-1].equals("X"))
+            return false;
+        }
+        
+        if(row < 3 && room[row+2][column].equals("P") && !room[row+1][column].equals("X") )return false;
+        if(row > 1 && room[row-2][column].equals("P") && !room[row-1][column].equals("X"))return false;
+        if(column < 3 && room[row][column+2].equals("P") && !room[row][column+1].equals("X"))return false;
+        if(column > 1 && room[row][column-2].equals("P") && !room[row][column-1].equals("X"))return false;
+        
+        
+        
+        return true;
+    }
     
     
-    private static boolean bfs(int row, int count, String[] p) {
-        System.out.println("bfs in, row : " +row+", count : "+count+", p : " + Arrays.toString(p));
-        
-        int dr[] = { -1, 1, 0, 0 }; // 다음 row
-        int dc[] = { 0, 0, -1, 1 }; // 다음 count
-
-        Queue<int[]> queue = new LinkedList<int[]>();
-        
-        queue.offer(new int[] { row, count }); // 현재 행렬을 큐에 추가
-        
-        while (!queue.isEmpty()) {
-            int[] position = queue.poll();
-            System.out.println("position >> " + Arrays.toString(position)); // 처음 0,0
-            
-            // 4방향을 검색
-            for (int i = 0; i < 4; i++) {
-                int nr = position[0] + dr[i];
-                int nc = position[1] + dc[i];
-                System.out.println("nr : " + nr + ", nc : " + nc); // 처음 0,0
-
-                // 이전 경우의 수가 없거나, 이후 경우의 수가 5보다 큰 경우 무시
-                if (nr < 0 || nc < 0 || nr >= 5 || nc >= 5 || (nr == row && nc == count))
-                    continue;
-
-                int d = Math.abs(nr - row) + Math.abs(nc - count); // 맨하탄거리 계산
-
-                if (p[nr].charAt(nc) == 'P' && d <= 2) {
-                    return false;
+    int dfs(String[][] room){
+        for(int r = 0 ; r < 5 ; r++ ){
+            for(int c = 0; c < 5; c ++){
+                Boolean ch = check(room, r, c);
+                if(!ch){
+                   return 0;  
                 }
-                else if (p[nr].charAt(nc) == 'O' && d < 2) {
-                    queue.offer(new int[] { nr, nc });
-                }
-            } // for end
-        } // while end  
-         return true;
-    } // bfs end
-} // class end
+            }
+        }
+        return 1;
+    } 
+
+}
--- week12/RoadOfJourney.java
+++ week12/RoadOfJourney.java
@@ -4,4 +4,43 @@ import java.util.*;
 
 public class RoadOfJourney {
 
+	boolean[] isVisited;
+    ArrayList<String> allpaths = new ArrayList<>();
+    
+    public String[] solution(String[][] tickets) {  
+        
+        isVisited = new boolean[tickets.length]; // 체크여부
+        String[] answer = {};
+        
+        dfs(tickets,"ICN","ICN",0); // 시작점인 ICN, 방문공항 역시 ICN 유일
+        
+        Collections.sort(allpaths); //  배열의 sort
+    //    System.out.println("dfs out : " + allpaths);    
+        answer = allpaths.get(0).split(",");
+        return answer;
+    }	
+	
+	public void dfs(String[][] tickets,String start,String path,int count){
+        
+		  //    System.out.println("dfs in, start: " + start + ", path: " + path + ", count: "+count);
+		        
+		        // dfs 진행 중 더해진 count값이 ticket의 전체 length와 동일할 경우, dfs out
+		        if(count == tickets.length){    
+		            allpaths.add(path);
+		            return;
+		        }
+		        
+		        for(int i = 0; i< tickets.length;i++){
+		            
+		            // 처음 start로 넘어온 값과 일치하는 출발지 발견시
+		            if(!isVisited[i] && tickets[i][0].equals(start)){
+		                isVisited[i] = true;
+		                
+		                // 그 다음 dfs 실행, path에 도착경로 추가, count 1 증가
+		                dfs(tickets, tickets[i][1], path + "," + tickets[i][1], count+1);
+		                isVisited[i] = false;
+		                
+		            }
+		       } 
+	}
 }    
